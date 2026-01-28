cfdf5fdeb47eb0701705c564c1c5463fbccf6cd6
loid345 <volanar@tut.by>
2026-01-28 19:39:45 +0300

    Implement core GraphQL schema

--- apps/server/src/controllers/graphql.rs
+++ apps/server/src/controllers/graphql.rs
@@ -1,36 +1,18 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
 use axum::{extract::State, routing::get, Extension, Json};
 use loco_rs::prelude::*;
 
 use crate::context::TenantContext;
-
-#[derive(Default)]
-pub struct Query;
-
-#[Object]
-impl Query {
-    async fn health(&self) -> &str {
-        "GraphQL is working!"
-    }
-
-    async fn version(&self) -> &str {
-        env!("CARGO_PKG_VERSION")
-    }
-}
-
-pub type AppSchema = Schema<Query, EmptyMutation, EmptySubscription>;
-
-fn build_schema() -> AppSchema {
-    Schema::build(Query, EmptyMutation, EmptySubscription).finish()
-}
+use crate::extractors::auth::OptionalCurrentUser;
+use crate::graphql::{build_schema, AppSchema};
 
 async fn graphql_handler(
     State(ctx): State<AppContext>,
     Extension(schema): Extension<AppSchema>,
     tenant_ctx: TenantContext,
+    OptionalCurrentUser(current_user): OptionalCurrentUser,
     Json(req): Json<async_graphql::Request>,
 ) -> Json<async_graphql::Response> {
-    let request = req.data(ctx).data(tenant_ctx);
+    let request = req.data(ctx).data(tenant_ctx).data(current_user);
     Json(schema.execute(request).await)
 }
 
